d636690498ee5e3df2881254493823d6aab74198
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-04-17 15:19:54 +0200

    finished classes with Book

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 "use strict";
 class Book {
+    constructor(title) {
+        this.title = title;
+    }
+    read() {
+        return `I'm reading ${this.title}!`;
+    }
 }
-class PaperBook {
+class PaperBook extends Book {
+    constructor(title, pages) {
+        super(title);
+        this.pages = pages;
+    }
+    read() {
+        return `I'm reading a paper book!`;
+    }
 }
-class ElectronicBook {
+class ElectronicBook extends Book {
+    constructor(title, size) {
+        super(title);
+        this.size = size;
+    }
+    read() {
+        return `I'm reading a digital book!`;
+    }
 }
-class ParchmentBook {
+class ParchmentBook extends Book {
+    constructor(title, length) {
+        super(title);
+        this.length = length;
+    }
 }
 class ReadBook {
+    constructor(books) {
+        this.books = books;
+    }
+    read() {
+        return this.books.read();
+    }
 }
 const phisicalBook = new PaperBook("Dragones de papel", 350);
 const digitalBook = new ElectronicBook("One piece", 150);
--- app.ts
+++ app.ts
@@ -1,12 +1,59 @@
-class Book{}
+class Book{
+    title:string; //todos los hijos comparten esta propiedad
 
-class PaperBook{}
+    constructor(title:string) { // si inicializan las propiedades
+        this.title = title;
+    }
 
-class ElectronicBook{}
+    read(){ //se crea el método
+        return `I'm reading ${this.title}!`;
+    }
+}
 
-class ParchmentBook{}
+class PaperBook extends Book{
+    pages:number;
 
-class ReadBook{}
+    constructor(title:string, pages:number){
+        super(title);
+        this.pages = pages;
+    }
+
+    read(){
+        return `I'm reading a paper book!`;
+    }
+}
+
+class ElectronicBook extends Book{
+    size:number;
+
+    constructor(title:string, size:number){
+        super(title);
+        this.size = size;
+    }
+
+    read(){
+        return `I'm reading a digital book!`;
+    }
+}
+
+class ParchmentBook extends Book{
+    length:number;
+
+    constructor(title:string, length:number){
+        super(title);
+        this.length = length;
+    }
+}
+
+class ReadBook{
+    books:Book;
+    constructor(books:Book){
+        this.books = books; //this.books = new Book ("One piece",150) si no tuvieramos la inyección de dependencias.
+    }
+    read(){
+        return this.books.read();
+    }
+}
 
 
 const phisicalBook = new PaperBook("Dragones de papel",350);
